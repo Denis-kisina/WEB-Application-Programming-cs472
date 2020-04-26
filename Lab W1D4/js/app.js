498cd67a04e1a7b8f63efea89f57292437554d30
Denis Kisina <kisinad@gmail.com>
2020-04-26 03:33:14 -0500

    added the checked property to the webform2

--- Lab W1D4/js/app.js
+++ Lab W1D4/js/app.js
@@ -3,8 +3,9 @@ function submitForm() {
     let textURL = document.getElementById("url");
     let textEmail = document.getElementById("email");
     let textPassword = document.getElementById("password");
+    const checkMeOut = document.getElementById("checkout");
 
-    console.log(` URL: ` + textURL.value +` ` + `Email ` + textEmail.value + ` ` + `Password ` + textPassword.value);
+    console.log(` URL: ` + textURL.value +` ` + `Email ` + textEmail.value + ` ` + `Password ` + textPassword.value + ` Checked? ` + checkMeOut.checked);
 };
 
 
