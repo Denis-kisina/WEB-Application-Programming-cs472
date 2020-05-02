46bd484c75677630010bc9b31fc47c79f58a095e
Denis Kisina <kisinad@gmail.com>
2020-05-02 04:34:33 -0500

    redid the w2d5 assignment using constructor functio and removed class implementation.

--- Week 2/W2D5 – assignment 9 - Using Inheritance JQuery and DOM/js/app.js
+++ Week 2/W2D5 – assignment 9 - Using Inheritance JQuery and DOM/js/app.js
@@ -1,5 +1,5 @@
 /**
- * W2D3 - Assignment 9
+ * W2D5 - Assignment 9
  * */
 "use strict";
 /*
@@ -26,20 +26,30 @@ const person = {
     },
     setName: function (newName) {
         this.name = newName;
+    },
+    setDateOfBirth: function(year, month, day) {
+        this.dateOfBirth = new Date(year,month,day);
+    },
+    getDateOfBirth: function () {
+        return this.dateOfBirth;
+    },
+    toString: function () {
+        return `{"name": "${this.name}", "dateOfBirth": "${this.dateOfBirth}"`;
     }
 };
 
 const printPersonalDetails = function () {
-    console.log(`The person’s name is ${this.getName()}`);
-    console.log(`${this.getName()} was born on ${this.dateOfBirth}`);
+    console.log(`The person’s name is ${this.getName()}\n`);
+    console.log(`${this.getName()} was born on ${ new Intl.DateTimeFormat().format(this.getDateOfBirth())}`);
 }
 const john = Object.create(person);
 john.setName("John");
-john.dateOfBirth = new Intl.DateTimeFormat('en-US').format(new Date(Date.UTC(1998, 11, 10,0,0)));
+john.setDateOfBirth(1998,12,10);
 
-
-const johnsPrintedPersonalDetails = printPersonalDetails.bind(john);
-johnsPrintedPersonalDetails();
+console.log(`The person’s name is ${john.getName()}\n`);
+console.log(`${john.getName()} was born on ${ new Intl.DateTimeFormat().format(john.getDateOfBirth())}`);
+// const johnsPrintedPersonalDetails = printPersonalDetails.bind(john);
+// johnsPrintedPersonalDetails();
 
 /*
 2.	Given that an Employee IS-A person, applying JavaScript inheritance and the Object.create(…) method, create a generic object named, employee, with the following properties:
@@ -56,50 +66,41 @@ johnsPrintedPersonalDetails();
     Note the console output should be: Anna is a Programmer who earns $249,995.50
 
  */
-class Person2  {
-    constructor() {
-        this.name = "";
-        this.dateOfBirth = "";
-    }
+const employee = Object.create(person);
+employee.salary = 0.0;
+employee.hireDate = new Date();
+employee.doJob = function (jobTitle) {
+    console.log(`${this.getName()} is a ${jobTitle} who earns \$${this.salary}`);
+}
 
+const anna = Object.create(employee);
+anna.setName("Anna");
+anna.salary = 249995.50;
+anna.doJob("Programmer");
 
-    getName () {
-        return this.name;
-    }
-    setName (newName) {
-        this.name = newName;
-    }
+/*
+    3.	Re-write the person object specification described in Question 1 above, but this time, using a Constructor Function named, Person. Then, add a method named, toString() to the person object, which return the string representation of the person object data in the format:
+    {Name: John, DateOfBirth: 1998-12-10}
+    Using your constructor function for the person object, create a person named, Peter, whose date of birth is November 10, 1985.
+    Print-out to the console, the information for the person named, Peter, using its toString() method.
+
+ */
+function Person (name, year, month, day) {
+        this.name = name;
+        this.dateOfBirth = new Date(year,month,day);
+        // this.toString = function () {
+        //     return `Name: ${this.name}, DateOfBirth: ${new Intl.DateTimeFormat().format(this.dateOfBirth)}`;
+        // }
+
 };
-
-
-class Employee2 extends Person2 {
-    constructor(jobTitle) {
-        super();
-        this.jobTitle = jobTitle;
-        this.salary = "0";
-        this.hireDate = new Intl.DateTimeFormat('en-US').format(new Date());
-    }
-    doJob () {
-        return `${this.jobTitle}`;
-    }
+Person.prototype.toString = function () {
+    return `Name: ${this.name}, DateOfBirth: ${new Intl.DateTimeFormat().format(this.dateOfBirth)}`;
 }
-// const employee = {
-//     salary: "0",
-//     hireDate: new Intl.DateTimeFormat('en-US').format(new Date()),
-//     doJob: function (jobTitle) {
-//         return `${jobTitle}`;
-//     }
-// };
-const printEmployeeDetails = function () {
-    console.log(`${this.getName()} is a ${this.doJob()} who earns \$${this.salary}`);
-}
-let Anna =  new Employee2("Programmer");
-Anna.salary = "249.995.50";
-Anna.setName("Anna");
+const peter = new Person("Peter", 1985,11,10);
+
+console.log(peter.toString());
 
 
-const printAnnasSalary = printEmployeeDetails.bind(Anna, "Programmer");
-printAnnasSalary();
 
 /*
 3.	Re-write the person object specification described in Question 1 above, but this time, using a Constructor Function named, Person. Then, add a method named, toString() to the person object, which return the string representation of the person object data in the format:
@@ -124,8 +125,7 @@ class Person3  {
         return `Name: ${this.name}, DateOfBirth: ${this.dateOfBirth}`;
     }
 };
-let john1 = new Person3("John", 1998,12,10);
-console.log(john1.toString());
+
 
 /*
 4.	Refer to your work on Lab Assignment 4. Add Javascript and JQuery code to work with your 2 HTML forms as follows:
