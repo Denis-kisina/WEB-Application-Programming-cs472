0e12a23d946d70654221af4a7a33b1a2f879499e
Denis Kisina <kisinad@gmail.com>
2020-05-08 07:50:20 -0500

    Added the hit counter to assignment 13.  Corrected the search for assignment 11 extra points

--- Week 3/Lesson13_JSPMVCDemo/src/edu/miu/cs/cs472/lesson13jspmvc/service/SearchedMessageService.java
+++ Week 3/Lesson13_JSPMVCDemo/src/edu/miu/cs/cs472/lesson13jspmvc/service/SearchedMessageService.java
@@ -4,6 +4,7 @@ import edu.miu.cs.cs472.lesson13jspmvc.model.ContactMessage;
 import edu.miu.cs.cs472.lesson13jspmvc.repository.ContactMessageRepository;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -13,11 +14,21 @@ public class SearchedMessageService {
     public SearchedMessageService() {
         this.contactMessageRepository =  new ContactMessageRepository();
     }
-
+    private boolean isPresent (String theItem, String str2Check){
+        Pattern checkRegex = Pattern.compile(theItem, Pattern.CASE_INSENSITIVE);
+        Matcher searchMatcher = checkRegex.matcher(str2Check);
+        if(!searchMatcher.find()){
+            return false;
+        }
+        return true;
+    }
     public List<ContactMessage> searchContactMessage(String item) {
+        Pattern p = Pattern.compile(".*item*.", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher("item");
+        boolean isMatch = m.matches();
+
         return this.contactMessageRepository.getContactMessages().stream()
-        Pattern.matches()
-                .filter(str -> str.getCategory().matches(".*[a-zA-Z]*."))
+                .filter(str -> isPresent(item, str.getCategory()) || isPresent(item, str.getCustomerName()) || isPresent(item, str.getGender()))
                 .collect(Collectors.toList());
     }
 }
